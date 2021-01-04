ff0519dc338feccb3fe752998e244eb31c93ec59
Roberto Robles <r.roblesfernandez@gmail.com>
2021-01-04 11:01:29 +0100

    use the sincricpro thermostat

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,80 @@
 #include <Arduino.h>
+#ifdef ESP8266
+  #include <ESP8266WiFi.h>
+#endif
+#ifdef ESP32
+  #include <WiFi.h>
+#endif
+
+#include <SinricPro.h>
+#include <SinricProThermostat.h>
+
+#include "credentials.h"
+
+
+const int BAUD_RATE = 9600;
+
+bool powerState;
+float setTemperature;
+
+
+bool onPowerState(const String &deviceId, bool &state) {
+  Serial.printf("Thermostat %s turned %s\r\n", deviceId.c_str(), state?"on":"off");
+  powerState = state; 
+  return true;
+}
+
+bool onTargetTemperature(const String &deviceId, float &temperature) {
+  Serial.printf("Thermostat %s set temperature to %f\r\n", deviceId.c_str(), temperature);
+  setTemperature = temperature;
+  return true;
+}
+
+bool onAdjustTargetTemperature(const String & deviceId, float &temperatureDelta) {
+  setTemperature += temperatureDelta;
+  Serial.printf("Thermostat %s changed temperature about %f to %f", deviceId.c_str(), temperatureDelta, setTemperature);
+  temperatureDelta = setTemperature;
+  return true;
+}
+
+bool onThermostatMode(const String &deviceId, String &mode) {
+  Serial.printf("Thermostat %s set to mode %s\r\n", deviceId.c_str(), mode.c_str());
+  return true;
+}
+
+
+void setupWiFi() {
+  Serial.printf("\r\n[Wifi]: Connecting");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.printf(".");
+    delay(250);
+  }
+  IPAddress localIP = WiFi.localIP();
+  Serial.printf("connected!\r\n[WiFi]: IP-Address is %d.%d.%d.%d\r\n", localIP[0], localIP[1], localIP[2], localIP[3]);
+}
+
+void setupSinricPro() {
+  SinricProThermostat &myThermostat = SinricPro[THERMOSTAT_ID];
+  myThermostat.onPowerState(onPowerState);
+  myThermostat.onTargetTemperature(onTargetTemperature);
+  myThermostat.onAdjustTargetTemperature(onAdjustTargetTemperature);
+  myThermostat.onThermostatMode(onThermostatMode);
+
+  // setup SinricPro
+  SinricPro.onConnected([](){ Serial.printf("Connected to SinricPro\r\n"); }); 
+  SinricPro.onDisconnected([](){ Serial.printf("Disconnected from SinricPro\r\n"); });
+  SinricPro.begin(APP_KEY, APP_SECRET);
+  SinricPro.restoreDeviceStates(true);
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(BAUD_RATE); Serial.printf("\r\n\r\n");
+  setupWiFi();
+  setupSinricPro();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  SinricPro.handle();
 }
